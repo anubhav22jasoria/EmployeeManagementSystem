7753532d40955a5b889c73c52f56c003c49cf826
Anubhav Jasoria <anubhavjasoria31@gmail.com>
2024-05-16 12:10:16 +0530

    added EmployeeDto and EmployeeMapper

---
+++ src/main/java/com/anubhav/emsbackend/mapper/EmployeeMapper.java
@@ -0,0 +1,25 @@
+package com.anubhav.emsbackend.mapper;
+
+import com.anubhav.emsbackend.dto.EmployeeDto;
+import com.anubhav.emsbackend.entity.Employee;
+
+public class EmployeeMapper {
+    public static EmployeeDto mapToEmployeeDto(Employee employee){
+        return new EmployeeDto(
+                employee.getId(),
+                employee.getFirstName(),
+                employee.getLastName(),
+                employee.getEmail()
+        );
+    }
+
+
+    public static Employee mapToEmployee(EmployeeDto employeeDto){
+        return new Employee(
+                employeeDto.getId(),
+                employeeDto.getFirstName(),
+                employeeDto.getLastName(),
+                employeeDto.getEmail()
+        );
+    }
+}
